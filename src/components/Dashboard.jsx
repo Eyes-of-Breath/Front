3d6c650f3dea7b1c7a3384c2a291a2bc6c467d99
JieunK <kje147459@gmail.com>
2025-07-02 00:02:28 +0900

    feat: update dashboard page layout

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,10 +1,19 @@
 import React, {  } from 'react';
+import './Dashboard.css';
 
 function Dashboard() {
 
     return (
-        <div>
-            <h1>This is dashboard page.</h1>
+        <div className='dashboard'>
+            <div className='leftside'>
+                <button type='button'>대시보드</button>
+                <button type='button'>이미지 분석</button>
+                <button type='button'>기록</button>
+                <button type='button'>프로필</button>
+            </div>
+            <div className='rightside'>
+                <h1>대시보드</h1>
+            </div>
         </div>
     );
 }
